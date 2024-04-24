11506f18663e7346a316d87a5041bc240ab76a69
Sverre <59171289+sverben@users.noreply.github.com>
2024-04-24 12:15:45 +0200

    style: enhanced user experience by showing what's happening (#2)

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,37 +1,43 @@
-import {createMaterialBottomTabNavigator} from "react-native-paper/react-navigation";
+import { createMaterialBottomTabNavigator } from "react-native-paper/react-navigation";
 import FeedScreen from "./feed/feed";
 import MediaScreen from "./media/media";
 import SettingsScreen from "./settings";
+import { useContext } from "react";
+import AuthContext, { Authed } from "../auth";
 
-const Tab = createMaterialBottomTabNavigator()
+const Tab = createMaterialBottomTabNavigator();
 
 export default function HomeScreen() {
-    return (
-        <Tab.Navigator>
-            <Tab.Screen
-                name={"Feed"}
-                component={FeedScreen}
-                options={{
-                    tabBarLabel: 'Home',
-                    tabBarIcon: 'home'
-                }}
-            />
-            <Tab.Screen
-                name={"Media"}
-                component={MediaScreen}
-                options={{
-                    tabBarLabel: 'Media',
-                    tabBarIcon: 'video'
-                }}
-            />
-            <Tab.Screen
-                name={"Settings"}
-                component={SettingsScreen}
-                options={{
-                    tabBarLabel: 'Instellingen',
-                    tabBarIcon: 'cog'
-                }}
-            />
-        </Tab.Navigator>
-    )
+  const authState = useContext(AuthContext);
+
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name={"Feed"}
+        component={FeedScreen}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: "home",
+        }}
+      />
+      {authState.authenticated === Authed.AUTHENTICATED && (
+        <Tab.Screen
+          name={"Media"}
+          component={MediaScreen}
+          options={{
+            tabBarLabel: "Media",
+            tabBarIcon: "video",
+          }}
+        />
+      )}
+      <Tab.Screen
+        name={"Settings"}
+        component={SettingsScreen}
+        options={{
+          tabBarLabel: "Instellingen",
+          tabBarIcon: "cog",
+        }}
+      />
+    </Tab.Navigator>
+  );
 }
